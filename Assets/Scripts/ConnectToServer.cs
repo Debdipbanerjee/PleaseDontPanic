e570821ec9aadf6533af1754d768d4dac6f7cae0
Debdipbanerjee <debdipbanerjee7@gmail.com>
2022-02-07 01:54:46 -0700

    Connected to server and main menu added

---
+++ Assets/Scripts/ConnectToServer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Photon.Pun;
+
+public class ConnectToServer : MonoBehaviourPunCallbacks
+{
+
+    public void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+        SceneManager.LoadScene("Main Menu");
+    }
+}
