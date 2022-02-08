806d99bc32941b87feefaf23af16428be68dd3da
Debdipbanerjee <debdipbanerjee7@gmail.com>
2022-02-08 00:34:34 -0700

    Player animation added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,10 +7,12 @@ public class PlayerController : MonoBehaviour
 {
     public float speed;
     PhotonView view;
+    Animator anim;
 
     private void Start()
     {
         view = GetComponent<PhotonView>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
@@ -21,6 +23,15 @@ public class PlayerController : MonoBehaviour
             Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
             Vector2 moveAmount = moveInput.normalized * speed * Time.deltaTime;
             transform.position += (Vector3)moveAmount;
+
+            if(moveInput == Vector2.zero)
+            {
+                anim.SetBool("isRunning", false);
+            }
+            else
+            {
+                anim.SetBool("isRunning", true);
+            }
         }
         
     }
